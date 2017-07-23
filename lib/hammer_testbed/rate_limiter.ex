2679d9ed5a86ddf50acfe80a29685c42a5ae9cf2
Shane Kilkelly <shane@kilkelly.me>
2017-07-23 14:35:04 +0100

    Update

--- lib/hammer_testbed/rate_limiter.ex
+++ lib/hammer_testbed/rate_limiter.ex
@@ -1,17 +1,23 @@
 defmodule HammerTestbed.RateLimiter do
   use Supervisor
 
+  # Use the Hammer module, creates `check_rate`, `inspect_bucket`,
+  # and `delete_buckets` functions in this module
+
+  use Hammer, backend: Hammer.Backend.ETS
+  # use Hammer, backend: Hammer.Backend.Redis
+
   def start_link() do
     Supervisor.start_link(__MODULE__, :ok)
   end
 
   def init(:ok) do
     children = [
-      # worker(Hammer.Backend.ETS, []),
-      worker(Hammer.Backend.Redis, []),
-      worker(Hammer, [[backend: Hammer.Backend.Redis,
-                       expiry_ms: 1000 * 60 * 2,
-                       cleanup_rate_ms: 1000 * 60 * 12]])
+      # Start the Hammer backend process
+      worker(Hammer.Backend.ETS, [[expiry_ms: 1000 * 60 * 10,
+                                   cleanup_rate_ms: 1000 * 60 * 2]]),
+      # worker(Hammer.Backend.Redis, [[expiry_ms: 1000 * 60 * 2,
+      #                                redix_config: []]]),
     ]
     supervise(children, strategy: :one_for_one, name: HammerTestbed.RateLimiter)
   end
--- lib/hammer_testbed/web/controllers/page_controller.ex
+++ lib/hammer_testbed/web/controllers/page_controller.ex
@@ -1,21 +1,26 @@
 defmodule HammerTestbed.Web.PageController do
   use HammerTestbed.Web, :controller
   import Logger
+  alias HammerTestbed.RateLimiter
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def get_timestamp(conn, _params) do
+    # Get a (semi) unique identifier for this user
     ip = conn.remote_ip
     |> Tuple.to_list
     |> Enum.join(".")
-    case Hammer.check_rate("get_timestamp:#{ip}", 60_000, 5) do
+    # Do the rate-limit check, permit five hits per minute
+    case RateLimiter.check_rate("get_timestamp:#{ip}", 60_000, 5) do
+      # Action is allowed
       {:allow, _count} ->
         Logger.log(:info, "Rate-Limit ok, generating timestamp")
         now = DateTime.utc_now()
-        conn
-        |> json(%{timestamp: "#{now}"})
+        conn |> json(%{timestamp: "#{now}"})
+      # Action is denied, because the rate-limit has been exceeded
+      # for this time-period
       {:deny, _} ->
         Logger.log(:info, "Rate-Limit exceeded, denying request")
         conn |> send_resp(429, "Too many requests")
