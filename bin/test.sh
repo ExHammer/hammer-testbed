c4d4750cb3226f1ee9aa0af1142e67edbc71648e
Shane Kilkelly <shane@kilkelly.me>
2017-07-30 12:44:43 +0100

    stuff

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
-#! env bash
+#! /usr/bin/bash
 
 for i in {1..12}
 do
-    echo $(curl -s http://localhost:4000/timestamp)
-    echo '    ' $(curl -s http://localhost:4000/timestamp/rl)
+    echo "$i =" $(curl -s http://localhost:4000/timestamp)
+    echo '       ' $(curl -s http://localhost:4000/timestamp/rl)
 done
