299ad72f1203ca1ef6e0ff7aea49b0e586b34d08
Shane Kilkelly <shane@kilkelly.me>
2017-08-01 18:39:34 +0100

    Default to the ETS backend

--- lib/hammer_testbed/rate_limiter.ex
+++ lib/hammer_testbed/rate_limiter.ex
@@ -4,8 +4,8 @@ defmodule HammerTestbed.RateLimiter do
   # Use the Hammer module, creates `check_rate`, `inspect_bucket`,
   # and `delete_buckets` functions in this module
 
-  # use Hammer, backend: Hammer.Backend.ETS
-  use Hammer, backend: Hammer.Backend.Redis
+  use Hammer, backend: Hammer.Backend.ETS
+  # use Hammer, backend: Hammer.Backend.Redis
 
   def start_link() do
     Supervisor.start_link(__MODULE__, :ok)
@@ -14,10 +14,10 @@ defmodule HammerTestbed.RateLimiter do
   def init(:ok) do
     children = [
       # Start the Hammer backend process
-      # worker(Hammer.Backend.ETS, [[expiry_ms: 1000 * 60 * 10,
-      #                              cleanup_rate_ms: 1000 * 60 * 2]]),
-      worker(Hammer.Backend.Redis, [[expiry_ms: 1000 * 60 * 2,
-                                     redix_config: []]]),
+      worker(Hammer.Backend.ETS, [[expiry_ms: 1000 * 60 * 10,
+                                   cleanup_rate_ms: 1000 * 60 * 2]]),
+      # worker(Hammer.Backend.Redis, [[expiry_ms: 1000 * 60 * 2,
+      #                                redix_config: []]]),
     ]
     supervise(children, strategy: :one_for_one, name: HammerTestbed.RateLimiter)
   end
